c36244799270033534f33f356295081f54db534d
StynlFear <82573820+StynlFear@users.noreply.github.com>
2023-03-20 11:31:50 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,24 @@ import { RegisterCompPage } from "./containers/Authentification/Register/Registe
 import { PageType } from "./containers/Authentification/Register/Choose/Choose.jsx";
 import { Forgotpassword } from "./containers/Authentification/Forgot password/forgotpassword.jsx";
 import { ResetPassword } from "./containers/Authentification/Reset password/resetpassword.jsx";
-import { CreateForm } from "./components/create_form_page/create_from.jsx";
-import { SideBar } from "./components/sidebar/sidebar.jsx";
-import { Formpage } from "./containers/Authentification/Formpage/Formpage.jsx";
-import Deloppop from "./components/delete option popup/deleteoptpop.jsx";
-import Deldyn from "./components/modal del dinam fields/modal.jsx";
-import Delsecpop from "./components/delete_section/delsecpop.jsx";
-import Delformpop from "./components/delete_form_pop/delformpop.jsx";
-import Attention from "./components/attention/attentionpop.jsx";
-import Generateurl from "./components/generate_url/generateurl.jsx";
-import { Pageuser } from "./components/page user/pageuser.jsx";
 import { Startup } from "./components/start up/startup.jsx";
 import CForm from "./containers/Create Form/CreateForm.jsx";
-import { Popup } from "./components/Popup/Popup.jsx";
-import { Editor } from '@tinymce/tinymce-react';
+
 import "./App.css";
 function App() {
   return (
     <div className="App">
-      <LoginPage/>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={< CForm/>} />
+          <Route exact path="/" element={<CForm />} />
           <Route path="Login" element={<LoginPage />} />
-          <Route path="RegisterInd" element={<RegisterIndPage/>} />
-          <Route path="RegisterComp" element={<RegisterCompPage/>} />
-          <Route path="RegisterPub" element={<RegisterPubPage/>} />
-          <Route path="Type" element={<PageType/>} />
-          <Route path="Forgot" element={<Forgotpassword/>} />
-          <Route path="Reset" element={<ResetPassword/>} />
-          <Route path="/test" element={<Startup/>} />
+          <Route path="RegisterInd" element={<RegisterIndPage />} />
+          <Route path="RegisterComp" element={<RegisterCompPage />} />
+          <Route path="RegisterPub" element={<RegisterPubPage />} />
+          <Route path="Type" element={<PageType />} />
+          <Route path="Forgot" element={<Forgotpassword />} />
+          <Route path="Reset" element={<ResetPassword />} />
+          <Route path="/test" element={<Startup />} />
         </Routes>
       </BrowserRouter>
     </div>
