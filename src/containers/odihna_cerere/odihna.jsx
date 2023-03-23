7289b8d3ca30d25a28e2e6725f82990f487afb99
AluculeseiAndrei <116576194+AluculeseiAndrei@users.noreply.github.com>
2023-03-24 00:23:05 +0200

    bug fixes

---
+++ src/containers/odihna_cerere/odihna.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./odihna.css"
+export default function Odihna() {
+
+  return (
+    <div>
+        <div style={{backgroundColor: '#DBEBFF', height: '300vh'}}>
+        <Sidebar/>
+        <div className="od-t1">Intelligent Forms</div>
+        <div className="od-t2">Form</div>
+        <div className="od-t3">Cerere pentru concediu de odihna</div>
+        <div className="od-cont1">
+            <div className="od-cont-t1">Section 1</div>
+            <hr className="od-bara"/>
+            <form action="">
+                <label className="labels-od1">NUME</label>
+                <input type="text" className="input-od"/>
+                <br />
+                <label className="labels-od1">FIRMA</label>
+                <input type="text" className="input-od"/>
+                <br />
+                <label className="labels-od1">FUNCTIE</label>
+                <input type="text" className="input-od"/>
+                <br />
+                <label className="labels-od1">NUMAR DE ZILE</label>
+                <input type="text" className="input-od"/>
+                <br />
+                <label className="labels-od1">PERIOADA</label>
+                <input type="text" className="input-od"/>
+                <br />
+                <label className="labels-od1">DATA</label>
+                <input type="text" className="input-od"/>
+                <br />
+                <button className="Sub-od">Submit</button>
+            </form>
+            <div className="od-cont2"></div>
+        </div>
+        </div>
+    </div>
+  );
+}
